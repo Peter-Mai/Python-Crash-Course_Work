be110a3d931772832b770f02625d2cf0b1ae91fe
Peter Mai <peter.mai1996@gmail.com>
2020-02-28 15:30:03 -0600

    Add files via upload

---
+++ seeing_the_world.py
@@ -0,0 +1,36 @@
+# 3.8 Seeing the World
+
+places = ['Vietnam', 'Japan', 'Korea', 'France', 'Singapore']
+print(places)
+
+#Temporarily sorting
+print(sorted(places))
+
+#Original list
+print(places)
+
+
+#Reverse order using sorted
+places.sort(reverse = True)
+print(places)
+
+
+#Original list to show it has stayed the same 
+print(places)
+
+#Reversing order of list
+places.reverse()
+print(places)
+
+#Reversing list back to original state 
+places.reverse()
+print(places)
+
+#Sorting list so it is stored in alphabetical order
+places.sort()
+print(places)
+
+#Sorting list to reverse alphabetical order
+places.sort(reverse = True)
+print(places)
+
