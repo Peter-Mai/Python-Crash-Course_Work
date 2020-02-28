48b056925ff4eadf9f36772371b915d809913b9f
Peter Mai <peter.mai1996@gmail.com>
2020-02-28 15:30:11 -0600

    Add files via upload

---
+++ dinner_guest.py
@@ -0,0 +1,13 @@
+# 3.9 Dinner Guest
+
+
+
+dinner = ['Tokido', 'JDCR', 'Mkleo']
+
+print(f"{dinner[0].title()}, I would like to invite you to dinner.")
+
+print(f"{dinner[1].title()}, I would like to invite you to dinner.")
+
+print(f"{dinner[2].title()}, I would like to invite you to dinner.")
+
+print('This many people will be invited to dinner: ', len(dinner))
