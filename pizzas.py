4a6b0b65bf5dc1cec739902e223a8ba1c598ff7d
Peter Mai <peter.mai1996@gmail.com>
2020-03-13 16:13:16 -0500

    Add files via upload

---
+++ pizzas.py
@@ -0,0 +1,11 @@
+#4.1 Pizza 
+
+pizzas = ['Meat lovers', 'Pepperoni', 'Buffalo Chicken']
+
+for pizza in pizzas:
+	print(f" I like ", pizza.title(), "pizza.")
+
+print("I really love pizza.")
+print("Meat lovers is my favorite.")
+print("Buffaclo chicken pizza is my second favorite.")
+print("And I still like pepporoni pizza!")
