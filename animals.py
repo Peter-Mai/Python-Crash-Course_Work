1ab2d36c2dc33d052eae8408ce2d939f7b1c45fa
Peter Mai <peter.mai1996@gmail.com>
2020-03-13 16:13:25 -0500

    Add files via upload

---
+++ animals.py
@@ -0,0 +1,7 @@
+#4.2 Animals
+animals = ['dogs', 'wolves', 'puppies']
+
+for animal in animals: 
+	print(f"A ", animal.title(), "would make a great pet!")
+
+print("Any of these animals are considered canine!")
